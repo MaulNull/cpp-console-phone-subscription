bf882ce9a3d81c3a2d2fa462a3c6dc79e0dd4945
MaulNull <gomail1138@gmail.com>
2025-05-31 02:44:32 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #0. Library Subscription\n";
     cout << "Author: Sergey Yermochenko\n";
+    cout << "Group: XX\n";
     return 0;
 }
